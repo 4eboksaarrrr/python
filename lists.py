7e7ba6e0e4d7af8f5b7067a0222467980827bc14
4eboksaarrrr <mz0762913@gmail.com>
2025-03-10 17:26:23 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,45 @@
+#-------------------- 1 доюавление в список(метод append) ---------------------
+#
+# list_number = [1, 2, 3, 4, 5]
+# for i in range(5):
+#     new_num = int(input("введите числа"))
+#     list_number.append(new_num)
+# print(list_number)
+
+#----------------------индексы-----------------------
+#
+# scores = [8, 5, 10, 7, 6]
+# scores[1] *= 3
+# x = scores[0]
+#
+# print(x)
+# print(scores)
+
+# slovo = "kakaska"
+# zamena = 'L'
+# list1 = list(slovo)
+#
+# # for i in slovo:
+# #     list1.append(i)
+#
+# # list1[2] = zamena
+# # print(list1)
+
+#---------- --------------------метод insert-------------------------
+# list_1 = ['popa ', 'kaka', 'kal']
+# list_1.insert(2,'govno')
+# print(list_1)
+
+#------------------------метод index--------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
